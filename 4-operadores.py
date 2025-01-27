9abc9109162e1444c6d3143307f646d59dc40940
“AdriDevs23” <adridevs23@gmail.com>
2025-01-27 17:08:28 +0100

    Cuarto ejercicio actualizado y acabado

--- 4-operadores.py
+++ 4-operadores.py
@@ -9,10 +9,12 @@ Reemplaza el ? con el operador según sea el caso
 """
 num1 = 10
 num2 = 5
-# print(f"Suma: {num1 ? num2}")
-# print(f"Resta: {num1 ? num2}")
-# print(f"Multiplicación: {num1 ? num2}")
-# print(f"División: {num1 ? num2}")
+print(f"El resultado de la suma es: {num1 + num2}")
+print(f"El resultado de la resta es: {num1 - num2}")
+print(f"El resultado de la multiplicación es: {num1 * num2}")
+print(f"El resultado de la división es: {num1 / num2}")
+
+
 """
 --- Ejercicio 2 operadores lógicos  ---
 Reemplaza el ? con el operador según sea el caso
@@ -24,19 +26,20 @@ michi_vuela = False
 michi_interesado = True
 michi_juega = True
 michi_pone_huevos = False
-# print(f"AND: {michi_4_patas ? michi_3_ojos}")
-# print(f"OR: {michi_interesado ? michi_vuela}")
-# print(f"NOT bool1: {? michi_juega}")
-# print(f"NOT bool2: {? michi_pone_huevos}")
+
+print(f"AND: {michi_4_patas} AND {michi_3_ojos}")
+print(f"OR: {michi_interesado} OR {michi_vuela}")
+print(f"NOT bool1: NOT {michi_juega}")
+print(f"NOT bool2: NOT {michi_pone_huevos}")
 """
 --- Ejercicio 3 Comparadores  ---
 Reemplaza el ? con el comparador según sea el caso
 """
 a = 7
 b = 3
-# print(f"¿a es igual a b?: {a ? b}")
-# print(f"¿a es diferente de b?: {a ? b}")
-# print(f"¿a es mayor que b?: {a ? b}")
-# print(f"¿a es menor que b?: {a ? b}")
-# print(f"¿a es mayor o igual que b?: {a ? b}")
-# print(f"¿a es menor o igual que b?: {a ? b}")
+print(f"¿a es igual a b?: {a == b}")
+print(f"¿a es diferente de b?: {a != b}")
+print(f"¿a es mayor que b?: {a > b}")
+print(f"¿a es menor que b?: {a < b}")
+print(f"¿a es mayor o igual que {b}?: {a >= b}")
+print(f"¿a es menor o igual que {b}?: {a <= b}")
