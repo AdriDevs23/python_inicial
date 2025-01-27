ace2febf5ebc4d15251b95d493cf9eea41e7e909
“AdriDevs23” <adridevs23@gmail.com>
2025-01-27 14:56:27 +0100

    Actualizacion del tercer ejercicio

--- 3-colecciones.py
+++ 3-colecciones.py
@@ -3,11 +3,14 @@
 En este taller aprenderás a manipular coleccciones de datos: Listas, diccionarios, tuplas y sets.
 """
 """
- --- LISTAS ---
+    --- LISTAS ---
 Las listas son ordenadas y mutables.
 Pueden contener elementos duplicados.
 Puedes modificar, añadir y eliminar elementos.
 """
+
+
+
 """
 --- Ejercicio 1 Listas ---
 Crea una variable "mascotas" que almacene una lista con los siguientes elementos: 'perro', 'gato', 'loro'
@@ -15,17 +18,31 @@ Imprime por consola el valor almacenado
 Despues haz los pasos pedidos
 """
 # Escribe tu código aquí
+mascotas = ["perro", "gato", "loro"]
 # Escribe el código para saber la cantidad de elementos que tiene la lista, imprimir por consola
+print(f"La lista {mascotas} contiene {len(mascotas)} elementos.")
 # Escribe el código para acceder al valor de la posición 2, imprimir por consola
+print(f"La posicion numero 2 contiene al animal {mascotas[1]}")
 # Escribe el código para agregar una elemento a la lista, imprimir por consola la lista
+mascotas.insert(1, "pajaros")
+print(f"A la lista de {mascotas} le hemos añadido un animal en la segunda posicion")
 # Escribe el código para modificar un elemento de la lista, imprimir por consola la lista
+mascotas[1] = "gaviota"
+print(mascotas)
 # Escribe el código para eliminar un elemento de la lista, imprimir por consola la lista
+mascotas.remove("perro")
+print(mascotas)
+
+
+
 """
- --- TUPLAS ---
+    --- TUPLAS ---
 Las tuplas son ordenadas e inmutables.
 Pueden contener elementos duplicados.
 No puedes modificar, añadir o eliminar elementos después de la creación.
 """
+
+
 """
 --- Ejercicio 2 Tuplas ---
 Crea una variable "plantas" que almacene una tupla con los siguientes elementos: 'cactus', 'orquidea', 'rosas'
@@ -38,12 +55,16 @@ Despues haz los pasos pedidos
 # Intentar modificar una tupla
 # plantas[1] = 'hoja rota'  # Descomenta esta línea para ver qué sucede
 # Escribe tu análisís acá acerca de qué sucede
+
+
 """
- --- SETS ---
+    --- SETS ---
 Los sets son desordenados y mutables.
 No pueden contener elementos duplicados.
 Puedes añadir y eliminar elementos, pero no puedes modificar los elementos existentes.
 """
+
+
 """
 --- Ejercicio 3 Sets ---
 Crea una variable "nombres" que almacene un set con los siguientes elementos: 'María', 'Cris', 'Cris', 'Alex'
@@ -57,11 +78,16 @@ Haz los pasos pedidos
 # Escribe el código para agregar una elemento al set, imprimir por consola el set
 # Escribe el código para eliminar un elemento del set, imprimir por consola el set
 """
+
+
+
 --- DICCIONARIOS ---
 Los diccionarios son desordenados y mutables.
 Contienen pares clave-valor.
 Puedes añadir, modificar y eliminar pares clave-valor.
 """
+
+
 """
 --- Ejercicio 4 Diccionarios ---
 Crea un diccionario llamado "ciudad" con las claves 'nombre' y 'pais' y los valores 'Barcelona' y 'España' respectivamente.
